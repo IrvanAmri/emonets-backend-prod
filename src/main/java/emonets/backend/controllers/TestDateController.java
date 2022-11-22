447c02d6be5cb8cf88bf79570c3b58015befb1d9
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-11-22 19:55:00 +0700

    Update TestDateController.java
    clean warning

--- src/main/java/emonets/backend/controllers/TestDateController.java
+++ src/main/java/emonets/backend/controllers/TestDateController.java
@@ -1,15 +1,12 @@
 package emonets.backend.controllers;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import emonets.backend.dto.CobaDateData;
+
 import emonets.backend.dto.TransaksiData;
 import lombok.extern.slf4j.Slf4j;
 
